a50f9223d4580dd99e0fcdd30ad8d6507f08a314
lukianov <lukyanovvitaliymailbox@gmail.com>
2025-06-06 13:52:55 +0300

    build: base style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
-
+  <div class="w-full h-full justify-center flex items-center">
+    <Button>Test Button</Button>
   </div>
 </template>
+
+<script setup lang="ts">
+import Button from "primevue/button"
+</script>
 
 <style scoped>
 </style>
